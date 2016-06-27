e5bdccb233f2873901594edc566800d4decb8518
Jones Melton <shavah@gmail.com>
2016-06-27 17:40:52 -0500

    Add item-fits? function

--- src/menu_calculator/core.clj
+++ src/menu_calculator/core.clj
@@ -1,7 +1,21 @@
 (ns menu-calculator.core
-  (:require [menu-calculator.menu :refer [menu]])
+  (:require [menu-calculator.menu :refer [menu target-price]])
   (:gen-class))
 
 (defn -main
   [& args]
   (println args))
+
+(defn sum-order
+  "tallies the cost of an order"
+  [order]
+  (reduce + (vals order)))
+
+(defn item-fits?
+  "checks if given item fits in given order"
+  [order item]
+  (< target-price (sum-order (cons order item))))
+
+(defn find-combos
+  "finds combos"
+  [menu total])
