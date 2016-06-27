7cdcde79158f6d41487a03194cf2adc50974e4f2
Jones Melton <shavah@gmail.com>
2016-06-27 17:59:52 -0500

    Correct item-fits? function

--- src/menu_calculator/core.clj
+++ src/menu_calculator/core.clj
@@ -14,7 +14,8 @@
 (defn item-fits?
   "checks if given item fits in given order"
   [order item]
-  (< target-price (sum-order (cons order item))))
+  (< target-price (sum-order (apply assoc order item))))
+                    "BORKED wants k/v pair not vec of k/v"
 
 (defn find-combos
   "finds combos"
