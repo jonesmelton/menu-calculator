22c9715181442d5a786c432cb587fbd8e29c069f
Jones Melton <shavah@gmail.com>
2016-06-27 10:38:33 -0500

    Add menu loading functions

--- src/menu_calculator/core.clj
+++ src/menu_calculator/core.clj
@@ -1,6 +1,22 @@
 (ns menu-calculator.core
+  (:require [clojure.data.csv :as csv])
   (:gen-class))
+
+(defn load-menu [filename]
+  (slurp filename))
+
+(defn menu-items-to-vectors [menu]
+  (csv/read-csv menu))
 
 (defn -main
   [& args]
   (println "Hello, World!"))
+
+(def menu 
+  (menu-items-to-vectors (load-menu "resources/menu.txt")))
+
+(def target-price
+  ((first menu) 0))
+
+(defn menu-into-map [menu]
+  (into {} (rest menu)))
--- test/menu_calculator/core_test.clj
+++ test/menu_calculator/core_test.clj
@@ -5,3 +5,14 @@
             (deftest ci-test
               (testing "passing test for CI hook"
                 (is (= 0 0))))
+
+
+(deftest menu-loading 
+  (testing "loads menu from csv"
+    (is (string? (load-menu "resources/menu.txt"))))
+  
+  (testing "generates target price"
+    (is (= target-price "$15.05")))
+  
+  (testing "menu has items and prices as k/v pairs"
+    (is (=  ["mixed fruit" "$2.15"] (first (menu-into-map menu))) )))
