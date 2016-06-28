5f9d53f91ad785b842f5faf6e91aaa00978c48b3
Jones Melton <shavah@gmail.com>
2016-06-28 09:16:51 -0500

    Add price-check functions

--- src/menu_calculator/core.clj
+++ src/menu_calculator/core.clj
@@ -1,5 +1,6 @@
 (ns menu-calculator.core
-  (:require [menu-calculator.menu :refer [menu target-price]])
+  (:require [menu-calculator.menu :refer [menu target-price]]
+            [clojure.math.combinatorics :as c])
   (:gen-class))
 
 (defn -main
@@ -15,8 +16,23 @@
   "checks if given item fits in given order"
   [order item]
   (< target-price (sum-order (apply assoc order item))))
-                    "BORKED wants k/v pair not vec of k/v"
+
+(def flip-menu
+  "swaps the keys and values of the menu hmap"
+  (clojure.set/map-invert menu))
+
+(defn price-check
+  "checks if a set of prices totals to the correct cost"
+  [price-set]
+  (= target-price (reduce + price-set)))                  
+
+(defn gen-and-check-subs
+  "generates a lazy seq of selections and filters for correct total price"
+  [item-count]
+  (filter price-check (c/selections (vals menu) item-count)))
 
 (defn find-combos
   "finds combos"
   [menu total])
+
+
